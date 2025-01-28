43e3be3234425a9bc1fe2dee84d9e077f826a3f4
rakshithujwal11 <rakshithujwal11@gmail.com>
2025-01-28 13:25:22 +0530

    replaced Shop.html with pug templating engine

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.set("view", "pug");
+app.set("view engine", "pug");
+app.set("views", "views");
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,9 @@ const rootDir = require("../utils/path");
 const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  console.log("shop adminData===>", adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", products);
 });
 
 module.exports = router;
