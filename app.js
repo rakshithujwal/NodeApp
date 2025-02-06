33363330292d2fc490d17a4076afb9d1d64be09a
rakshithujwal11 <rakshithujwal11@gmail.com>
2025-02-06 13:20:51 +0530

    create user logic added

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,15 +20,39 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((error) => {
+      console.log("Failed To fetch user====>", error);
+    });
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then((result) => {
-    // console.log("sequelize Sync Result====>", result);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      User.create({ name: "Raksh", email: "raksh@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    console.log("User======>", user);
     app.listen(3000);
   })
   .catch((error) => {
